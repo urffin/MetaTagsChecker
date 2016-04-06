3f6373e9dec68db2b94f4516afa0a4b1f9430fa6
Igor <urffin2006@gmail.com>
2016-04-06 21:30:22 +0300

    add test project

---
+++ MetaTagsChecker/MetaTagsCheckerTest/TestCSV.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MetaTagsCheckerTest
+{
+    [TestClass]
+    public class TestCSV
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
