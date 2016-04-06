e228aa9afed37c7d90cf3c3b35affba1f857af51
Igor <urffin2006@gmail.com>
2016-04-06 21:30:09 +0300

    add class for parsing csv files

---
+++ MetaTagsChecker/MetaTagsCheckerLib/CSV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaTagsCheckerLib
+{
+    public class CSV
+    {
+        public bool HasHeader { get; private set; }
+        public char Separator { get; private set; }
+
+        public CSV(char separator = ';', bool hasHeader = true)
+        {
+            HasHeader = hasHeader;
+            Separator = separator;
+        }
+        public IEnumerable<string[]> Load(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                return Load(reader);
+            }
+        }
+        public IEnumerable<string[]> Load(TextReader reader)
+        {
+            reader.
+        }
+    }
+}
